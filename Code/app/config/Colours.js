fd2db604bb59c059368814c243700228bf727cda
Connor de Bruyn <debruconn@ecs.vuw.ac.nz>
2021-08-11 19:12:49 +1200

    Created a colours config file

---
+++ Code/app/config/Colours.js
@@ -0,0 +1,15 @@
+/**
+ * Colours that I will be using throughout the project
+ */
+export default {
+    //Nav Bar
+    blue_primary: '#329dfa',
+    blue_secondary: '#1fc1de',
+    purple_primary: '#9d41f2',
+    purple_secondary: '#815ae6',
+
+    //Standard Colours
+    black: '#000',
+    white: '#fff',
+    light_grey: '#cfcfcf',
+}
