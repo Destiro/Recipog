9c12e620e8b42ac98365638660126a7504855300
Connor de Bruyn <debruconn@ecs.vuw.ac.nz>
2021-09-05 23:23:55 +1200

    Basic Grocery data and flatlist created

--- Code/app/screens/GroceriesScreen.js
+++ Code/app/screens/GroceriesScreen.js
@@ -1,17 +1,32 @@
 import React, {useState} from 'react';
 import Colours from "../config/Colours";
-import { StyleSheet, Text, View } from 'react-native';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
 import Header from "../components/Header";
+import GroceryItem from "../components/GroceryItem";
 
 const GroceriesScreen = (props) =>  {
-    const [groceries, setGroceries] = useState([]);
-    //props.login
+    const [groceries, setGroceries] = useState([
+        {text: 'coffee', key: 'coffee'},
+        {text: 'cupcakes', key: 'cupcakes'},
+        {text: 'chocolate', key: 'chocolate'}
+    ]);
+
+    const pressHandler = (key) => {
+        setGroceries((prevGroceries) => {
+            return prevGroceries.filter(groceries => groceries.key != key)
+        })
+    }
 
     return (
         <View style={styles.container}>
             <Header title={"Groceries"} />
-            <View style={styles.list}>
-
+            <View style={styles.contentBox}>
+                <View style={styles.list}>
+                    <FlatList data={groceries}
+                              renderItem={({ item }) => (
+                                  <GroceryItem item={item} pressHandler={pressHandler}/>
+                              )} />
+                </View>
             </View>
         </View>
     );
@@ -21,11 +36,17 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-    },
-    list: {
         backgroundColor: Colours.light_grey,
+    },
+    contentBox: {
         width: '80%',
         height: '90%',
+        backgroundColor: Colours.white,
+    },
+    list: {
+        backgroundColor: Colours.white,
+        width: '100%',
+        height: '80%',
     }
 });
 
