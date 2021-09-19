64749ec8590fd0ffa16fa3c861e78cb6d7d7f77e
Connor de Bruyn <debruconn@ecs.vuw.ac.nz>
2021-09-20 00:50:06 +1200

    Added a second pass input and hide characters in signup page

--- Code/app/screens/SignupScreen.js
+++ Code/app/screens/SignupScreen.js
@@ -8,53 +8,68 @@ import {db} from "../config/FirebaseConfig";
 const SignupScreen = ({navigation}) => {
     const [login, setLogin] = React.useState('');
     const [password, setPassword] = React.useState('');
+    const [password2, setPassword2] = React.useState('');
 
     const ref = db.firestore().collection("Users");
 
     //Adds current inputted text as a new entry in firestore
-    function addLogin(username, password) {
-        if (login !== '' && password !== '') {
-            ref.doc(username).set({
-                username: username,
-                password: password,
-                ingredients: [],
-                groceries: [],
-                favourites: []
-            }).then(function () {
-                console.log("User successfully added!");
-            }).catch(function (error) {
-                console.error("Error adding user: ", error)
-            });
-
-            //Route back to login page
-            navigation.navigate("Login");
+    function addLogin(username, password, password2) {
+        if (password !== password2) {
+            alert("Passwords do not match!!")
         } else {
-            //An empty given
-            alert("Please enter a valid username and password! >:(");
+            if (login !== '' && password !== '') {
+                ref.doc(username).set({
+                    username: username,
+                    password: password,
+                    ingredients: [],
+                    groceries: [],
+                    favourites: []
+                }).then(function () {
+                    console.log("User successfully added!");
+                }).catch(function (error) {
+                    console.error("Error adding user: ", error)
+                });
+
+                //Route back to login page
+                navigation.navigate("Login");
+            } else {
+                //An empty given
+                alert("Please enter a valid username and password! >:(");
+            }
         }
     }
 
     return (
         <View style={styles.container}>
-            <View style={styles.headerStyle} />
+            <View style={styles.headerStyle}/>
             <View style={styles.SignupBox}>
                 <Text style={styles.title}> Create Account </Text>
-                <View style={{paddingBottom: 30, backgroundColor: Colours.purple_primary}} />
+                <View style={{paddingBottom: 20, backgroundColor: Colours.purple_primary}}/>
                 <TextInput
                     label="Username"
                     value={login}
                     onChangeText={text => setLogin(text)}
                     style={{backgroundColor: Colours.white}}
                 />
-                <View style={{paddingBottom: 30, backgroundColor: Colours.purple_primary}} />
+                <View style={{paddingBottom: 20, backgroundColor: Colours.purple_primary}}/>
                 <TextInput
                     label="Password"
                     value={password}
+                    secureTextEntry={true}
                     onChangeText={text => setPassword(text)}
                     style={{backgroundColor: Colours.white}}
                 />
-                <View style={{paddingBottom: 30, backgroundColor: Colours.purple_primary}} />
-                <Button title={"Create Account"} style={styles.button} onPress={() => addLogin(login, password)}  />
+                <View style={{paddingBottom: 20, backgroundColor: Colours.purple_primary}}/>
+                <TextInput
+                    label="Confirm Password"
+                    value={password2}
+                    secureTextEntry={true}
+                    onChangeText={text => setPassword2(text)}
+                    style={{backgroundColor: Colours.white}}
+                />
+                <View style={{paddingBottom: 20, backgroundColor: Colours.purple_primary}}/>
+                <Button title={"Create Account"} style={styles.button}
+                        onPress={() => addLogin(login, password, password2)}/>
             </View>
             <TouchableWithoutFeedback onPress={() => navigation.navigate("Login")}>
                 <View style={styles.signUpFooter}>
